fe45f86424ac8ed9455594fffbfed2a04ae09bf9
gabica <gpeicic2@gmail.com>
2025-01-20 21:16:52 +0100

    domacaZadaca- mali update

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
         LocalDateTime dt = LocalDateTime.now();
-        System.out.println(dt);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+
+        String formatiraniDatumIVrijeme = dt.format(formatter);
+
+        System.out.println(formatiraniDatumIVrijeme);
     }
 }
